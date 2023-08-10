23ee99b6d12dc26eefc07ef4302e2ee5b2ce9f47
KADIYAM SANTHOSH KUMAR <santhosh102002@gmail.com>
2023-08-10 19:25:41 +0530

    Connecting to MongodB

--- Config/dbConfig.js
+++ Config/dbConfig.js
@@ -1,14 +1,14 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const dbConnect = ()=>{
-// mongoose.connect(process.env.MONGO_URL)
-// .then((conn)=>{
-//     console.log(`Connected to db successfully ${conn.connection.host}`)
-// })
-// .catch(()=>{
-//     console.log('Failed to connect to db')
-// })
+const dbConnect = ()=>{
+mongoose.connect(process.env.MONGO_URL)
+.then((conn)=>{
+    console.log(`Connected to db successfully ${conn.connection.host}`)
+})
+.catch(()=>{
+    console.log('Failed to connect to db')
+})
 
-// }
+}
 
-// module.exports = dbConnect;
+module.exports = dbConnect;
